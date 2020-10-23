605c89459516cbad50bbc916f15b87c268be1315
Estauti <estautib@gmail.com>
2020-10-23 20:27:33 -0300

    Add user model specs

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :user do
-    
+    name { FFaker::Name.name }
+		email { FFaker::Internet.email }
+		password_attr = FFaker::Internet.password
+		password { password_attr }
+		password_confirmation { password_attr }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,92 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'on create' do
+    describe 'with valid attributes' do
+      let(:valid_attributes) { attributes_for(:user) }
+
+      it 'creates a user' do
+        expect do
+          create(:user, valid_attributes)
+        end.to change(User, :count).by(1)
+      end
+    end
+
+    describe 'is invalid' do
+      context 'when name' do
+        it 'is null' do
+          user = build(:user, name: nil)
+
+          expect(user).to be_invalid
+        end
+
+        it 'is empty' do
+          user = build(:user, name: '')
+
+          expect(user).to be_invalid
+        end
+      end
+
+      context 'when email' do
+        it 'is null' do
+          user = build(:user, email: nil)
+
+          expect(user).to be_invalid
+        end
+
+        it 'is empty' do
+          user = build(:user, email: '')
+
+          expect(user).to be_invalid
+        end
+
+        it 'has invalid format' do
+          user = build(:user, email: FFaker::Lorem.word)
+
+          expect(user).to be_invalid
+        end
+
+        it 'is already in use' do
+          old_user = create(:user)
+          user = build(:user, email: old_user.email)
+
+          expect(user).to be_invalid
+        end
+      end
+
+      context 'when password' do
+        it 'is null' do
+          user = build(:user, password: nil)
+
+          expect(user).to be_invalid
+        end
+
+        it 'is empty' do
+          user = build(:user, password: '')
+
+          expect(user).to be_invalid
+        end
+      end
+
+      context 'when password confirmation' do
+        it 'is null' do
+          user = build(:user, password_confirmation: nil)
+
+          expect(user).to be_invalid
+        end
+
+        it 'is empty' do
+          user = build(:user, password_confirmation: '')
+
+          expect(user).to be_invalid
+        end
+
+        it 'is differs from password' do
+          user = build(:user, password_confirmation: FFaker::Internet.password)
+
+          expect(user).to be_invalid
+        end
+      end
+    end
+  end
 end
